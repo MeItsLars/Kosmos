a37a2b823c0423dc3ff4f92c9d35048c7080a7d6
stirante <brzozowski.s.piotr@gmail.com>
2022-08-25 10:14:12 +0200

    Add missing go.sum and compile libraries to proper locations

--- goleveldb/build.sh
+++ goleveldb/build.sh
@@ -1,10 +1,10 @@
 # Windows
 echo Building for Windows
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="zig cc -target x86_64-windows-gnu" CXX="zig c++ -target x86_64-windows-gnu"  go build -ldflags="-s -w" -o windows-adm64/goleveldb.dll -buildmode=c-shared ./main.go
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="zig cc -target x86_64-windows-gnu" CXX="zig c++ -target x86_64-windows-gnu"  go build -ldflags="-s -w" -o ../src/main/resources/win32-x86_64/goleveldb.dll -buildmode=c-shared ./main.go
 # Linux
 echo Building for Linux
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC="zig cc -target x86_64-linux" CXX="zig c++ -target x86_64-linux"  go build -ldflags="-s -w" -o linux-amd64/goleveldb.so -buildmode=c-shared ./main.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC="zig cc -target x86_64-linux" CXX="zig c++ -target x86_64-linux"  go build -ldflags="-s -w" -o ../src/main/resources/linux-x86_64/goleveldb.so -buildmode=c-shared ./main.go
 # MacOS
 echo Building for MacOS
-CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC="zig cc -target x86_64-macos-gnu" CXX="zig c++ -target x86_64-macos-gnu"  go build -ldflags="-s -w" -o darwin-amd64/goleveldb.so -buildmode=c-shared ./main.go
-CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 CC="zig cc -target aarch64-macos-gnu" CXX="zig c++ -target aarch64-macos-gnu"  go build -ldflags="-s -w" -o darwin-arm64/goleveldb.so -buildmode=c-shared ./main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC="zig cc -target x86_64-macos-gnu" CXX="zig c++ -target x86_64-macos-gnu"  go build -ldflags="-s -w" -o ../src/main/resources/darwin-x86_64/goleveldb.so -buildmode=c-shared ./main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 CC="zig cc -target aarch64-macos-gnu" CXX="zig c++ -target aarch64-macos-gnu"  go build -ldflags="-s -w" -o ../src/main/resources/darwin-arm64/goleveldb.so -buildmode=c-shared ./main.go
