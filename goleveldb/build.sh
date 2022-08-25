9a7019fa829581f14e274b162c78aee60426a893
stirante <brzozowski.s.piotr@gmail.com>
2022-08-25 11:37:37 +0200

    Change MacOS library extension and exclude header files

--- goleveldb/build.sh
+++ goleveldb/build.sh
@@ -6,4 +6,4 @@ echo Building for Linux
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ../src/main/resources/linux-x86_64/goleveldb.so -buildmode=c-shared ./main.go
 # MacOS
 echo Building for MacOS
-CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ go build -ldflags="-s -w" -o ../src/main/resources/darwin-x86_64/goleveldb.so -buildmode=c-shared ./main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC=o64-clang CXX=o64-clang++ go build -ldflags="-s -w" -o ../src/main/resources/darwin-x86_64/goleveldb.dylib -buildmode=c-shared ./main.go
