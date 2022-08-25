4530f0d37f50754dde8b547d9b417c7b6c064538
stirante <brzozowski.s.piotr@gmail.com>
2022-08-25 13:26:10 +0200

    Fix library paths

--- src/main/java/nl/itslars/kosmos/leveldb/GoLevelDB.java
+++ src/main/java/nl/itslars/kosmos/leveldb/GoLevelDB.java
@@ -9,26 +9,6 @@ public class GoLevelDB {
     static {
         Native.register("goleveldb");
     }
-    /*
-extern __declspec(dllexport) void leveldb_free(void* ptr);
-extern __declspec(dllexport) char* leveldb_error();
-extern __declspec(dllexport) int leveldb_options_create();
-extern __declspec(dllexport) void leveldb_options_destroy(int id);
-extern __declspec(dllexport) void leveldb_options_set_compression(int id, unsigned int compression);
-extern __declspec(dllexport) int leveldb_open(char* path, int options);
-extern __declspec(dllexport) void leveldb_close(int id);
-extern __declspec(dllexport) int leveldb_iterator_create(int id);
-extern __declspec(dllexport) void leveldb_iterator_destroy(int id);
-extern __declspec(dllexport) int leveldb_iterator_next(int id);
-extern __declspec(dllexport) void leveldb_iterator_seek_to_first(int id);
-extern __declspec(dllexport) void* leveldb_iterator_key(int id, int* size);
-extern __declspec(dllexport) void* leveldb_iterator_value(int id, int* size);
-extern __declspec(dllexport) void* leveldb_get(int id, void* key, int keySize, int* valueSize);
-extern __declspec(dllexport) int leveldb_has(int id, void* key, int keySize);
-extern __declspec(dllexport) void leveldb_put(int id, void* key, int keySize, void* value, int valueSize);
-extern __declspec(dllexport) void leveldb_delete(int id, void* key, int keySize);
-extern __declspec(dllexport) void leveldb_repair(char* path);
-     */
 
     public static native void leveldb_free(Pointer ptr);
     public static native String leveldb_error();
